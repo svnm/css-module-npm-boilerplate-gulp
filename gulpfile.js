88650d84520b120f84ee36a9abd9082c58458add
Steven Iseki Martin <stevenisekimartin@gmail.com>
2016-02-06 15:22:03 +1100

    initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var less = require('gulp-less');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer
+    ];
+    return gulp.src('./src/*.less')
+        .pipe(less())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('default', ['css']);
